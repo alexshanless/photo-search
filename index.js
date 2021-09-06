eb017b5a382bb974db33175d94accd201479ae49
alexshanless <alex.shanless@gmail.com>
2021-09-06 16:52:39 -0700

    js done

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
 //api-key = 563492ad6f91700001000001317f7624faf84e319cad0c54c0d7ad98
 
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    // Typical action to be performed when the document is ready:
-    var res = JSON.parse(xhttp.responseText);
-    var imageData = res.photos.map(function (image) {
-      console.log(image);
-      return image;
-    });
+var searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      // Typical action to be performed when the document is ready:
+      var res = JSON.parse(xhttp.responseText);
+      var imageData = res.photos.map(function (image) {
+        console.log(image);
+        return image;
+      });
 
-    var container = document.querySelector("#container");
-    imageData.forEach((image) => {
-      var imageDiv = document.createElement("div");
-      imageDiv.innerHTML = `
-      <img src=${image.src.original}>
+      var container = document.querySelector("#container");
+      container.innerHTML = "";
+      imageData.forEach((image) => {
+        var imageDiv = document.createElement("div");
+        imageDiv.classList.add("image-div");
+        imageDiv.innerHTML = `
+      <img src=${image.src.medium}>
+      <p>${image.photographer}</p>
       `;
-      container.appendChild(imageDiv);
-    });
-  }
-};
-xhttp.open(
-  "GET",
-  "https://api.pexels.com/v1/search?query=nature&per_page=1",
-  true
-);
-xhttp.setRequestHeader(
-  "Authorization",
-  "563492ad6f91700001000001317f7624faf84e319cad0c54c0d7ad98"
-);
-xhttp.send();
+        container.appendChild(imageDiv);
+      });
+    }
+  };
+  var textValue = document.querySelector("#search-bar").value;
+  xhttp.open(
+    "GET",
+    `https://api.pexels.com/v1/search?query=${textValue}&per_page=9`,
+    true
+  );
+  xhttp.setRequestHeader(
+    "Authorization",
+    "563492ad6f91700001000001317f7624faf84e319cad0c54c0d7ad98"
+  );
+  xhttp.send();
+});
